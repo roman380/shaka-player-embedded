45d8aa4b6bfe1d82d5f8a3ab3e62774e6eeec05c
Roman Ryltsov <roman@alax.info>
2019-05-08 19:36:59 +0300

    Commented out (temporarily?) incompatible POSIX file system related calls

--- shaka/src/util/file_system_posix.cc
+++ shaka/src/util/file_system_posix.cc
@@ -14,10 +14,10 @@
 
 #include "src/util/file_system.h"
 
-#include <dirent.h>
-#include <libgen.h>
+//#include <dirent.h>
+//#include <libgen.h>
 #include <sys/stat.h>
-#include <unistd.h>
+//#include <unistd.h>
 
 #include <glog/logging.h>
 
@@ -47,18 +47,18 @@ std::string FileSystem::DirName(const std::string& path) {
   // null char at the last slash.  When it returns the argument, the string
   // constructor will copy the string up to that null char.
   std::string copy = path;
-  return dirname(&copy[0]);
+  return copy; //dirname(&copy[0]);
 }
 
 bool FileSystem::FileExists(const std::string& path) const {
   // Cannot use fstream for this since it allows opening directories.
   struct stat info;
-  return stat(path.c_str(), &info) == 0 && S_ISREG(info.st_mode);
+  return stat(path.c_str(), &info) == 0;// && S_ISREG(info.st_mode);
 }
 
 bool FileSystem::DirectoryExists(const std::string& path) const {
   struct stat info;
-  return stat(path.c_str(), &info) == 0 && S_ISDIR(info.st_mode);
+  return stat(path.c_str(), &info) == 0;// && S_ISDIR(info.st_mode);
 }
 
 bool FileSystem::DeleteFile(const std::string& path) const {
@@ -66,41 +66,41 @@ bool FileSystem::DeleteFile(const std::string& path) const {
 }
 
 bool FileSystem::CreateDirectory(const std::string& path) const {
-  std::string::size_type pos = 0;
-  while ((pos = path.find(kDirectorySeparator, pos + 1)) != std::string::npos) {
-    if (mkdir(path.substr(0, pos).c_str(), 0755) != 0 && errno != EEXIST)
+  //std::string::size_type pos = 0;
+  //while ((pos = path.find(kDirectorySeparator, pos + 1)) != std::string::npos) {
+  //  if (mkdir(path.substr(0, pos).c_str(), 0755) != 0 && errno != EEXIST)
       return false;
-  }
-
-  return mkdir(path.c_str(), 0755) == 0 || errno == EEXIST;
+  //}
+  //
+  //return mkdir(path.c_str(), 0755) == 0 || errno == EEXIST;
 }
 
 bool FileSystem::ListFiles(const std::string& path,
                            std::vector<std::string>* files) const {
-  files->clear();
-
-  DIR* dir;
-  dirent* entry;
-  if ((dir = opendir(path.c_str())) == nullptr) {
-    PLOG(ERROR) << "Unable to open directory '" << path << "'";
+  //files->clear();
+  //
+  //DIR* dir;
+  //dirent* entry;
+  //if ((dir = opendir(path.c_str())) == nullptr) {
+  //  PLOG(ERROR) << "Unable to open directory '" << path << "'";
     return false;
-  }
-
-  while ((entry = readdir(dir)) != nullptr) {
-    if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name))
-      continue;
-
-    const std::string sub_path = PathJoin(path, entry->d_name);
-    struct stat info;
-    CHECK_EQ(0, stat(sub_path.c_str(), &info));
-    if (S_ISREG(info.st_mode))
-      files->push_back(entry->d_name);
-    else if (!S_ISDIR(info.st_mode))
-      LOG(WARNING) << "Unable to process folder entry '" << sub_path << "'";
-  }
-  closedir(dir);
-
-  return true;
+  //}
+  //
+  //while ((entry = readdir(dir)) != nullptr) {
+  //  if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name))
+  //    continue;
+  //
+  //  const std::string sub_path = PathJoin(path, entry->d_name);
+  //  struct stat info;
+  //  CHECK_EQ(0, stat(sub_path.c_str(), &info));
+  //  if (S_ISREG(info.st_mode))
+  //    files->push_back(entry->d_name);
+  //  else if (!S_ISDIR(info.st_mode))
+  //    LOG(WARNING) << "Unable to process folder entry '" << sub_path << "'";
+  //}
+  //closedir(dir);
+  //
+  //return true;
 }
 
 }  // namespace util
